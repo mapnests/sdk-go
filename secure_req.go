6542e59f7a52101e8b41d876b854e533a3eed6a8
MapNests <developer@mapnests.com>
2025-06-25 17:11:06 +0600

    Added GO SDK
    Signed-off-by: MapNests <developer@mapnests.com>

---
+++ secure_req.go
@@ -0,0 +1,21 @@
+package sdk
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func callSecureRequest(label, apiKey, origin string, request any) (int, string, string, error) {
+	jsonBytes, err := json.Marshal(request)
+	if err != nil {
+		return 0, "", "", fmt.Errorf("failed to marshal request: %v", err)
+	}
+
+	result := performSecureRequest(label, apiKey, origin, string(jsonBytes))
+
+	if !result.Success {
+		return result.StatusCode, "", result.ErrorMessage, fmt.Errorf("secure request failed")
+	}
+
+	return result.StatusCode, result.Response, "", nil
+}
---
+++ secure_request.go
@@ -0,0 +1,104 @@
+package sdk
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"time"
+)
+
+type SecureResult struct {
+	Success      bool
+	StatusCode   int
+	Response     string
+	ErrorMessage string
+}
+
+var HTTPMethodMap = map[string]string{
+	"reverse":                 http.MethodGet,
+	"search":                  http.MethodGet,
+	"distanceMatrix":          http.MethodGet,
+	"distanceMatrixDetails":   http.MethodGet,
+	"pairWiseRouteSummary":    http.MethodPost,
+	"multiSourceRouteSummary": http.MethodPost,
+}
+
+var token string
+
+func performSecureRequest(label, apiKey, origin, jsonRequest string) SecureResult {
+
+	urlStr, err := buildURLFromJSON(label, jsonRequest)
+	if err != nil {
+		return SecureResult{false, 0, "", err.Error()}
+	}
+
+	method := HTTPMethodMap[label]
+	if method == "" {
+		method = http.MethodGet
+	}
+
+	tokenHeader, err := generateToken(apiKey)
+	if err != nil {
+		return SecureResult{false, 0, "", err.Error()}
+	}
+
+	headers := map[string]string{
+		"X-API-KEY":    apiKey,
+		"Origin":       origin,
+		"fxsrf":        tokenHeader,
+		"Content-Type": "application/json",
+	}
+
+	client := &http.Client{Timeout: 30 * time.Second}
+	var resp *http.Response
+
+	makeRequest := func() (*http.Response, error) {
+		var req *http.Request
+		if method == http.MethodPost {
+			req, err = http.NewRequest(http.MethodPost, urlStr, bytes.NewBuffer([]byte(jsonRequest)))
+		} else {
+			req, err = http.NewRequest(http.MethodGet, urlStr, nil)
+		}
+		if err != nil {
+			return nil, err
+		}
+		for k, v := range headers {
+			req.Header.Set(k, v)
+		}
+		return client.Do(req)
+	}
+
+	resp, err = makeRequest()
+	if err != nil {
+		return SecureResult{false, 0, "", err.Error()}
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return SecureResult{false, resp.StatusCode, "", err.Error()}
+	}
+
+	if resp.StatusCode == http.StatusUnauthorized {
+		h := resp.Header.Get("cf-ray-status-id-tn")
+		if h != "" {
+			token = doubleBase64Encode(h)
+			tokenHeader, _ = generateToken(apiKey)
+			headers["fxsrf"] = tokenHeader
+
+			resp, err = makeRequest()
+			if err != nil {
+				return SecureResult{false, 0, "", err.Error()}
+			}
+			defer resp.Body.Close()
+			bodyBytes, _ = io.ReadAll(resp.Body)
+		}
+	}
+
+	return SecureResult{
+		Success:      resp.StatusCode >= 200 && resp.StatusCode < 300,
+		StatusCode:   resp.StatusCode,
+		Response:     string(bodyBytes),
+		ErrorMessage: "",
+	}
+}
